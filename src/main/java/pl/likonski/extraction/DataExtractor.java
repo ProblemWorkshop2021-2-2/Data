804cb9dc4bfec9841d68e7048472d42f043f3136
Klaudia-24 <klaudia.winer@gmail.com>
2021-06-06 14:38:02 +0200

    Added code for new data file and some changes

--- src/main/java/pl/likonski/extraction/DataExtractor.java
+++ src/main/java/pl/likonski/extraction/DataExtractor.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 public class DataExtractor {
@@ -23,6 +24,14 @@ public class DataExtractor {
             commits = Arrays.stream(readFromFile("commits.csv", "$")).map(com -> new Commit(com[0], com[1], com[2], com[3],
                     Integer.parseInt(com[4]), com[5], com[6],
                     Integer.parseInt(com[7]))).toArray(Commit[]::new);
+
+            Arrays.sort(commits, new Comparator<Commit>() {
+                @Override
+                public int compare(Commit o1, Commit o2) {
+                    return o1.compareTo(o2);
+                }
+            });
+
         } catch (NumberFormatException e){
             e.printStackTrace();
         }
@@ -67,9 +76,11 @@ public class DataExtractor {
 
         try {
             insertionsCommits = Arrays.stream(readFromFile("insertions_commits.csv", ","))
-                    .map(com -> new InsertionsCommits(com[0], com[1], Integer.parseInt(com[2].trim().split(" ")[0]),
-                            Integer.parseInt(com[3].trim().split(" ")[0]),
-                            Integer.parseInt(com[4].trim().split(" ")[0]))).toArray(InsertionsCommits[]::new);
+                    .map(com -> new InsertionsCommits(com[0],
+                            com[1],
+                            (com.length>=5?Integer.parseInt(com[2].trim().split(" ")[0]):0),
+                            (com.length>=5?Integer.parseInt(com[3].trim().split(" ")[0]):0),
+                            (com.length>=5?Integer.parseInt(com[4].trim().split(" ")[0]):0))).toArray(InsertionsCommits[]::new);
 
         } catch (NumberFormatException e){
             e.printStackTrace();
@@ -81,7 +92,12 @@ public class DataExtractor {
         String[][] values = null;
         try {
             rows = Files.readString(Paths.get(dirPath + fileName)).split("[" + System.getProperty("line.separator") + "]");
-            values = Arrays.stream(rows).skip(1).map(row -> row.split("[" + separator + "]")).toArray(String[][]::new);
+            if(fileName.equals("insertions_commits.csv")){
+                values = Arrays.stream(rows).skip(2).map(row -> row.split("[" + separator + "]")).toArray(String[][]::new);
+            }
+            else {
+                values = Arrays.stream(rows).skip(1).map(row -> row.split("[" + separator + "]")).toArray(String[][]::new);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
